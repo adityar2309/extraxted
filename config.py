ba69cadd84b22bcd745cb879691ffef7b1eab706
Aditya R <adityar2309@gmail.com>
2025-11-09 14:36:30 +0530

    Change API credentials in config.py
    Updated API credentials and token in Config class.

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6216840269:AAFNzApyRlXuZ94T3UZgd-XqNun2mA40kJU")
-    API_ID = int(os.environ.get("API_ID", "7856100"))
-    API_HASH = os.environ.get("API_HASH", "dba409d7e8d44ab5bb689378deea1792")
-    AUTH_USERS = "1411895712"
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7161201979:AAH1yNFQcNFQOWgCwT2-byP_xyQLgofv4XQ")
+    API_ID = int(os.environ.get("API_ID", "20937775"))
+    API_HASH = os.environ.get("API_HASH", "a01276c8c21178e56c4a962b56203db7")
+    AUTH_USERS = "5166775158"
+
 
